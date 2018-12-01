877e8b7a09c9856ed1b9f17dc2a01190473722ef
Amanda Reilly <amandamichellereilly@gmail.com>
2018-12-01 13:44:29 -0800

    Descope files.

--- src/components/containers/pageSection/main.js
+++ src/components/containers/pageSection/main.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
-import InvestorDashboard from '../view/investorDashboard';
-import InvestorCompanyDetail from '../view/investorCompanyDetail';
 import CompanyProfile from '../view/companyProfile';
 import IssueShares from '../view/issueShares';
 import InvestmentListing from '../view/investmentListing';
 
-import { dashboardData, companyDetailData, modalData, purchaseFormData, companyProfileData, investmentListingData, requestListData } from '../../_temporaryData';
+import { companyProfileData, investmentListingData, requestListData } from '../../_temporaryData';
 
 import '../../../css/components/containers/pageSection/main.scss';
 
@@ -16,16 +14,6 @@ export default function Main() {
         <main className="main-container">
             <Router>
                 <Switch>
-                    <Route
-                        exact path='/dashboard'
-                        render={(props) => <InvestorDashboard {...props} cardData={dashboardData.investmentCards} summaryData={dashboardData.summaryData} />}
-                    />
-                    <Route
-                        exact path='/investorCompanyDetail'
-                        render={(props) => <InvestorCompanyDetail {...props} data={companyDetailData}
-                        modalData={modalData}
-                        purchaseFormData={purchaseFormData} />}
-                    />
                     <Route
                         exact path='/'
                         render={(props) => <CompanyProfile {...props} data={companyProfileData} requestListData={requestListData} />}
