74d4cdf246b24fc73fb7684d19fbde4648acc25e
Amanda Reilly <amandamichellereilly@gmail.com>
2018-12-06 16:58:41 -0800

    Style form defaults.

--- src/components/forms/renderField.js
+++ src/components/forms/renderField.js
@@ -7,13 +7,13 @@ export function renderInputField({
     meta: { touched, error, warning }
 }) {
     return (
-        <div className="field">
-            <label className="label">{label}</label>
-            <div className="control">
-                <input {...input} placeholder={label} type={type} />
+        <div className={`field${(touched && error ? ' error' : (touched && warning ? 'warning' : ''))}`}>
+            <label className={`label${(touched && error ? ' error' : (touched && warning ? 'warning' : ''))}`}>{label}</label>
+            <div className={`input-wrapper${(touched && error ? ' error' : (touched && warning ? 'warning' : ''))}`}>
+                <input {...input} placeholder={label} type={type} className="input" />
                 {touched &&
-                    ((error && <span>{error}</span>) ||
-                        (warning && <span>{warning}</span>))}
+                    ((error && <span className="form-field-error error">{error}</span>) ||
+                        (warning && <span className="warning form-field-error">{warning}</span>))}
             </div>
         </div>
     );
@@ -27,7 +27,7 @@ export function renderSelectField({
     children
 }) {
     return(
-        <div className="field">
+        <div className={`field${(touched && error ? ' error' : (touched && warning ? 'warning' : ''))}`}>
             <label className="label">{label}</label>
             <div className="control">
                 <select {...input}>
